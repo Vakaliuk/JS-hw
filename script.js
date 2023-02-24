67905b310c6b331ac15e2d5c8f3cd55782964bf1
Vakaliuk <69085662+Vakaliuk@users.noreply.github.com>
2023-02-24 19:30:47 +0200

    add first script with math operations

---
+++ script.js
@@ -0,0 +1,7 @@
+let num1 = prompt('Введіть перше число');
+let num2 = prompt('Введіть друге число');
+
+alert('Сума:' + ' ' + (Number(num1) + Number(num2)));
+alert('Різниця:' + ' ' + (Number(num1) - Number(num2)));
+alert('Множення:' + ' ' + Number(num1) * Number(num2));
+alert('Ділення:' + ' ' + Number(num1) / Number(num2));
