45694d7d4d886928babd9e6fd3ed99b32205caa6
Vakaliuk <69085662+Vakaliuk@users.noreply.github.com>
2023-02-24 21:12:11 +0200

    try if for math

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-let num1 = prompt('Введіть перше число');
-let num2 = prompt('Введіть друге число');
+let num1 = Number(prompt('Введіть перше число'));
+let num2 = Number(prompt('Введіть друге число'));
 
-alert('Сума:' + ' ' + (Number(num1) + Number(num2)));
-alert('Різниця:' + ' ' + (Number(num1) - Number(num2)));
-alert('Множення:' + ' ' + Number(num1) * Number(num2));
-alert('Ділення:' + ' ' + Number(num1) / Number(num2));
+if (isNaN(num1) || isNaN(num2)) {
+  alert('Потрібно вводити число');
+} else if (num1 == '' || num2 == '') {
+  alert('Помилка! Поле не може бути пустим');
+} else {
+  alert('Сума:' + ' ' + (num1 + num2));
+
+  if (num1 < num2) {
+    if (confirm('Ви впевненні, що хочете продовжити операцію?')) {
+      alert('Різниця:' + ' ' + (num1 - num2));
+    }
+  }
+
+  alert('Множення:' + ' ' + num1 * num2);
+
+  if (num2 == 0) {
+    alert('Ділення: На нуль ділити не можна');
+  } else {
+    alert('Ділення:' + ' ' + num1 / num2);
+  }
+}
