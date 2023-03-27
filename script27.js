468daad59e407994c0c0b391d31b5fd621eafeb3
Vakaliuk <69085662+Vakaliuk@users.noreply.github.com>
2023-03-27 18:40:54 +0300

    hw27 v.2 - prompt

--- script27.js
+++ script27.js
@@ -1,100 +1,77 @@
-let arr = [
-  8,
-  false,
-  22,
-  147,
-  62,
-  -28,
-  12,
-  -55,
-  '2',
-  true,
-  'qweqwe',
-  5,
-  -7,
-  9,
-  null,
-];
-console.log(arr);
+const titleResultRef = document.querySelector('.titleResult');
+const showResult = document.createElement('p');
+titleResultRef.append(showResult);
 
-// ============================================
-// show arr
-const containerRef = document.querySelector('.minmax');
-const showArr = document.createElement('p');
-
-showArr.textContent = arr;
-containerRef.prepend(`actual arr: ${arr}`);
-
-// ==========================================
-// calc number: prepare
 let num1;
 let num2;
-let result;
+function checkNum() {
+  num1 = prompt('Введіть перше число');
+  if (num1 === '') {
+    alert(`Помилка, Ви не ввели перше число`);
+    return;
+  } else if (num1 === null) {
+    return;
+  }
 
-const input1 = document.getElementById('num1');
-const input2 = document.getElementById('num2');
+  num2 = prompt('Введіть друге число');
+  if (num2 === '') {
+    alert(`Помилка, Ви не ввели друге число`);
+    return;
+  } else if (num2 === null) {
+    return;
+  }
 
-const sumBtn = document.getElementById('sumBtn');
-const subBtn = document.getElementById('subBtn');
-
-sumBtn.addEventListener('click', () => {
-  num1 = input1.value;
-  num2 = input2.value;
-  resultSum();
-});
-
-subBtn.addEventListener('click', () => {
-  num1 = input1.value;
-  num2 = input2.value;
-  resultSub();
-});
-
-// calc number: SUMM foo
-const sum = function sumNumber(num1, num2) {
   if (isNaN(num1) || isNaN(num2)) {
     return alert('Потрібно вводити число');
-  } else if (num1 == '' || num2 == '') {
+  } else if (num1 === '' || num2 === '') {
     return alert('Помилка! Поле не може бути пустим');
-  } else {
-    result = Number(num1) + Number(num2);
-    return result;
   }
-};
+  return true;
+}
 
-const resultSum = function () {
-  resultNum.textContent = sum(num1, num2);
+function summ() {
+  if (checkNum()) {
+    let result = Number(num1) + Number(num2);
 
-  let pColor = colorTextContent();
-  resultNum.style.color = pColor;
-
-  resultRef.append(resultNum);
-};
-
-// calc number: SUBT foo
-const sub = function sumNumber(num1, num2) {
-  if (isNaN(num1) || isNaN(num2)) {
-    return alert('Потрібно вводити число');
-  } else if (num1 == '' || num2 == '') {
-    return alert('Помилка! Поле не може бути пустим');
-  } else {
-    result = Number(num1) - Number(num2);
-    return result;
+    showResult.textContent = result;
+    colorTextContent();
   }
-};
+}
 
-const resultSub = function () {
-  resultNum.textContent = sub(num1, num2);
+function diff() {
+  if (checkNum()) {
+    let result = num1 - num2;
 
-  let pColor = colorTextContent();
-  resultNum.style.color = pColor;
+    showResult.textContent = result;
+    colorTextContent();
+  }
+}
 
-  resultRef.append(resultNum);
-};
+function mult() {
+  if (checkNum()) {
+    let result = num1 * num2;
 
-// ====================================
+    showResult.textContent = result;
+    colorTextContent();
+  }
+}
+
+function div() {
+  if (checkNum()) {
+    let result = num1 / num2;
+
+    showResult.textContent = result;
+    colorTextContent();
+  }
+}
+
+// ==================================================
 // find minmax
-let minNumber = 0;
-let maxNumber = 0;
+let arr = [8, false, 22, 147, -28, 12, -55, '2', true, 'qweqwe', -7, 9, null];
+
+let minNumber = Infinity;
+let maxNumber = -Infinity;
+let pColor;
 
 const max = function findMaxNumber(arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -118,57 +95,50 @@ const min = function findMinNumber(arr) {
   return minNumber;
 };
 
-// ================================
-// show Result
-const resultRef = document.querySelector('.titleResult');
-const resultNum = document.createElement('p');
-
 const resultMax = function () {
-  resultNum.textContent = max(arr);
+  showResult.textContent = max(arr);
 
-  let pColor = colorTextContent();
-  resultNum.style.color = pColor;
+  colorTextContent();
 
-  resultRef.append(resultNum);
+  titleResultRef.append(showResult);
 };
 
 const resultMin = function () {
-  resultNum.textContent = min(arr);
+  showResult.textContent = min(arr);
 
-  let pColor = colorTextContent();
-  resultNum.style.color = pColor;
+  colorTextContent();
 
-  resultRef.append(resultNum);
+  titleResultRef.append(showResult);
 };
 
 // =================================
 // foo for color
-const colorTextContent = function () {
-  if (resultNum.textContent > 0 && resultNum.textContent < 25) {
-    return 'black';
-  } else if (resultNum.textContent > 0 && resultNum.textContent < 25) {
-    return 'hsl(115, 100%, 50%)';
-  } else if (resultNum.textContent > 25 && resultNum.textContent < 50) {
-    return 'hsl(115, 100%, 40%)';
-  } else if (resultNum.textContent > 50 && resultNum.textContent < 75) {
-    return 'hsl(115, 100%, 30%)';
-  } else if (resultNum.textContent > 75 && resultNum.textContent < 100) {
-    return 'hsl(115, 100%, 25%)';
-  } else if (resultNum.textContent > 100 && resultNum.textContent < 150) {
-    return 'hsl(115, 100%, 20%)';
-  } else if (resultNum.textContent > 150) {
-    return 'hsl(115, 100%, 15%)';
-  } else if (resultNum.textContent < 0 && resultNum.textContent > -25) {
-    return 'hsl(0, 100%, 50%)';
-  } else if (resultNum.textContent < -25 && resultNum.textContent > -50) {
-    return 'hsl(0, 100%, 40%)';
-  } else if (resultNum.textContent < -50 && resultNum.textContent > -75) {
-    return 'hsl(0, 100%, 30%)';
-  } else if (resultNum.textContent < -75 && resultNum.textContent > -100) {
-    return 'hsl(0, 100%, 25%)';
-  } else if (resultNum.textContent < -100 && resultNum.textContent > -150) {
-    return 'hsl(0, 100%, 20%)';
-  } else if (resultNum.textContent < -150) {
-    return 'hsl(0, 100%, 15%)';
+function colorTextContent() {
+  if (showResult.textContent == 0) {
+    return (showResult.style.color = 'black');
+  } else if (showResult.textContent > 0 && showResult.textContent < 25) {
+    return (showResult.style.color = 'hsl(115, 100%, 50%)');
+  } else if (showResult.textContent > 25 && showResult.textContent < 50) {
+    return (showResult.style.color = 'hsl(115, 100%, 40%)');
+  } else if (showResult.textContent > 50 && showResult.textContent < 75) {
+    return (showResult.style.color = 'hsl(115, 100%, 30%)');
+  } else if (showResult.textContent > 75 && showResult.textContent < 100) {
+    return (showResult.style.color = 'hsl(115, 100%, 25%)');
+  } else if (showResult.textContent > 100 && showResult.textContent < 150) {
+    return (showResult.style.color = 'hsl(115, 100%, 20%)');
+  } else if (showResult.textContent > 150) {
+    return (showResult.style.color = 'hsl(115, 100%, 15%)');
+  } else if (showResult.textContent < 0 && showResult.textContent > -25) {
+    return (showResult.style.color = 'hsl(0, 100%, 50%)');
+  } else if (showResult.textContent < -25 && showResult.textContent > -50) {
+    return (showResult.style.color = 'hsl(0, 100%, 40%)');
+  } else if (showResult.textContent < -50 && showResult.textContent > -75) {
+    return (showResult.style.color = 'hsl(0, 100%, 30%)');
+  } else if (showResult.textContent < -75 && showResult.textContent > -100) {
+    return (showResult.style.color = 'hsl(0, 100%, 25%)');
+  } else if (showResult.textContent < -100 && showResult.textContent > -150) {
+    return (showResult.style.color = 'hsl(0, 100%, 20%)');
+  } else if (showResult.textContent < -150) {
+    return (showResult.style.color = 'hsl(0, 100%, 15%)');
   }
-};
+}
