a3a25d1d07d3c96cc10135cb99bb6f73837a940d
Vakaliuk <69085662+Vakaliuk@users.noreply.github.com>
2023-04-12 19:02:45 +0300

    add timer v.2

--- timeLeft.js
+++ timeLeft.js
@@ -1,21 +1,66 @@
+//---------timer v.1
+
+const divRef = document.querySelector('.container');
+
+const textTimeLeft = document.createElement('h2');
+textTimeLeft.style = 'color: white; text-shadow: 0px 0px 15px white';
+
 const newYearDate = new Date('2024-01-01T23:59:59');
+
+divRef.appendChild(textTimeLeft);
 
 const timeLeftToNewYear = setInterval(() => {
   const now = new Date().getTime();
   const timeLeft = newYearDate - now;
 
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  const days = pad(Math.floor(timeLeft / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
   );
-  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  const minutes = pad(Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)));
+  const seconds = pad(Math.floor((timeLeft % (1000 * 60)) / 1000));
 
-  showTimeLeft.textContent = `${days} days ${hours} hours ${minutes} minutes and ${seconds} seconds `;
+  textTimeLeft.textContent = `${days} days ${hours} hours ${minutes} minutes and ${seconds} seconds `;
 }, 1000);
 
-const divRef = document.querySelector('.container');
-const showTimeLeft = document.createElement('h2');
-showTimeLeft.style = 'color: red; text-shadow: 2px 2px 5px black';
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
 
-divRef.appendChild(showTimeLeft);
+//---------timer v.2
+
+// const divRef = document.querySelector('.container');
+// const textTimeLeft = document.createElement('h2');
+// textTimeLeft.style = 'color: white; text-shadow: 0px 0px 15px white';
+
+// const timeToNewYear = {
+//   start() {
+//     const newYearDate = new Date('2024-01-01T23:59:59');
+
+//     setInterval(() => {
+//       const currentTime = Date.now();
+//       const timeLeft = newYearDate - currentTime;
+
+//       showTimeLeft(timeLeft);
+//     }, 1000);
+//   },
+// };
+
+// function showTimeLeft(time) {
+//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+//   const hours = pad(
+//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+//   );
+//   const minutes = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//   const seconds = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+//   textTimeLeft.textContent = `${days} days ${hours} hours ${minutes} minutes and ${seconds} seconds `;
+// }
+
+// timeToNewYear.start();
+
+// divRef.appendChild(textTimeLeft);
+
+// function pad(value) {
+//   return String(value).padStart(2, '0');
+// }
